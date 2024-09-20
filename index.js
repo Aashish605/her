d403d8efa3303b75662654f8d47a524c070f02e0
Aashish605 <130918292+Aashish605@users.noreply.github.com>
2024-09-20 20:59:51 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let audio = document.querySelector('.audio')
+audio.style.display = 'none'
+setInterval(() => {
+    audio.style.display = 'block'
+}, 5000);
+// let boq = document.querySelector('.boq')
+// boq.style.display = 'none'
+// setInterval(() => {
+//     audio.style.display = 'block'
+// }, 5000);
